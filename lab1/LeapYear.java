e90974a4503182c757337497a5078f251cae21c2
ShawnLi1014 <shawnli1014@gmail.com>
2019-01-23 22:02:43 +0800

    lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,18 @@ public class LeapYear {
         }
     }
 
+
+    /** Check if the given year is a Leapyear */
+    public static boolean isLeapYear(int year){
+        if(year % 400 == 0){
+            return true;
+        }
+        else if(year % 4 == 0 && year % 100 != 0){
+            return true;
+        }
+        return false;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {

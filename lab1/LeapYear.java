9ab936cd322a9a8c26033ebc463570a20b616723
ShawnLi1014 <shawnli1014@gmail.com>
2019-01-24 20:56:36 +0800

    code style modified

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -16,11 +16,10 @@ public class LeapYear {
 
 
     /** Check if the given year is a Leapyear */
-    public static boolean isLeapYear(int year){
-        if(year % 400 == 0){
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
             return true;
-        }
-        else if(year % 4 == 0 && year % 100 != 0){
+        } else if (year % 4 == 0 && year % 100 != 0) {
             return true;
         }
         return false;
@@ -42,4 +41,3 @@ public class LeapYear {
         }
     }
 }
-
